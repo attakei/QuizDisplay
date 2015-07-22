398c7618471749125ad396337a22d57482cdde81
attakei <attakei@gmail.com>
2015-07-22 22:27:31 +0900

    ProgressComponentもjade化

--- src/progress/component.jsx
+++ src/progress/component.jsx
@@ -16,41 +16,10 @@ ViewControlPanel = React.createClass({
     render: ViewControlPanel_jade
 });
 
+ProgressComponent_jade = require("jade-react!./ProgressComponent.jade");
 ProgressComponent = React.createClass({
     mixins: [Arda.mixin, ProgressController],
-    render: function(){
-        var players = [];
-        var self = this;
-        this.props.players.map(function(player){
-            players.push(<Player player={player} tryAnswer={self.tryAnswer} />);
-        });
-        return (
-    <div>
-    <nav className="navbar navbar-default">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <p className="navbar-brand"><strong>{this.props.programName}</strong></p>
-        </div>
-
-        <div className="collapse navbar-collapse">
-          <ul className="nav navbar-nav navbar-right">
-            <li><a>{this.props.quizCount} 問経過</a></li>
-            <li><a>menu</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <div className="container">
-      <div className="row">
-      　{players}
-      </div>
-      <JudgePanel resetAnswer={this.resetAnswer} answerRight={this.answerRight} answerWrong={this.answerWrong} throughAnswer={this.throughAnswer} />
-      {/*<ViewControlPanel />*/}
-    </div>
-
-    </div>
-        );
-    }
+    render: ProgressComponent_jade
 });
 
 module.exports = ProgressComponent
